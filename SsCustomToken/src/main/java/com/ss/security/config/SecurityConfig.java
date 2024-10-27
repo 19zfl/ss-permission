7bc87e6ad3108f8e7fe1eebd0ce974993a53306b
zfl19 <19aleiya@gmail.com>
2024-10-27 15:38:57 +0800

    配置自定义异常处理（认证异常处理、授权异常处理）

--- SsCustomToken/src/main/java/com/ss/security/config/SecurityConfig.java
+++ SsCustomToken/src/main/java/com/ss/security/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.annotation.Resource;
@@ -28,6 +30,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Resource
     private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
 
+    @Resource
+    private AuthenticationEntryPoint authenticationEntryPoint;
+
+    @Resource
+    private AccessDeniedHandler accessDeniedHandler;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -44,8 +52,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable()//关闭csrf
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//不通过Session获取SecurityContext
                 .and().authorizeRequests().antMatchers("/user/login").anonymous()//对于登录接口允许匿名访问
+                .antMatchers("/dept/list").hasAuthority("system:dept:list")//配置中设置某个接口的权限
                 .anyRequest().authenticated();//除了上面外的所有请求全部需要鉴权认证
 
+        // 添加过滤器
         http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+        // 配置自定义异常处理
+        http.exceptionHandling()
+                // 配置认证异常处理
+                .authenticationEntryPoint(authenticationEntryPoint)
+                // 配置授权异常处理
+                .accessDeniedHandler(accessDeniedHandler);
+
+        // 允许跨域
+        http.cors();
     }
 }
