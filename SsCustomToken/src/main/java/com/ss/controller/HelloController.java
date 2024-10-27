4ac88bf901fcc43e931951f0cc7d980a0358c055
zfl19 <19aleiya@gmail.com>
2024-10-27 15:37:52 +0800

    增加测试接口

--- SsCustomToken/src/main/java/com/ss/controller/HelloController.java
+++ SsCustomToken/src/main/java/com/ss/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.ss.controller;
 
+import com.ss.utils.ResponseResult;
+import org.apache.coyote.Response;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @RequestMapping("/custom")
+    @PreAuthorize("@ss.hasAuthority('system:dept:list')")
+    public ResponseResult custom() {
+        return new ResponseResult(200, "访问成功", null);
+    }
 
     @RequestMapping("/dept")
     @PreAuthorize("hasAuthority('system:dept:list')")
