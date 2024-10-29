e4908dfa131510c4daaeeb5e36d8bcb571592c04
zfl19 <19aleiya@gmail.com>
2024-10-29 09:46:47 +0800

    配置注销成功处理器

--- SsQuickStart/src/main/java/com/ss/config/SecurityConfig.java
+++ SsQuickStart/src/main/java/com/ss/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.ss.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.annotation.Resource;
 
@@ -18,9 +20,17 @@ import javax.annotation.Resource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Resource
     private AuthenticationSuccessHandler authenticationSuccessHandler;
+    @Resource
+    private AuthenticationFailureHandler authenticationFailureHandler;
+    @Resource
+    private LogoutSuccessHandler logoutSuccessHandler;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().successHandler(authenticationSuccessHandler);
+        http.formLogin()
+                .successHandler(authenticationSuccessHandler)// 配置认证成功
+                .failureHandler(authenticationFailureHandler);// 配置认证失败
+        http.logout()
+                .logoutSuccessHandler(logoutSuccessHandler);// 配置退出登录处理器
 //        http.authorizeRequests().anyRequest().authenticated();
         http.authorizeRequests((requests -> requests.anyRequest().authenticated()));
     }
